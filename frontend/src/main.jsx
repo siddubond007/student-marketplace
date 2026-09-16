5d41384ee4322a7c0397176f65bf9e0d8f45e6d9
bond007always <siddusiddharth80193@gmail.com>
2026-09-16 16:54:39 +0530

    feat: enable HP-02 motion polish

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/home-foundation.css'
 import './styles/home-dual-separator.css'
+import './styles/home-dual-motion-polish.js'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
